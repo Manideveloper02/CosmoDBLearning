40bfd03202e479253466cdaaaf6d1a782dffc9b3
Manideveloper02 <64354799+Manideveloper02@users.noreply.github.com>
2021-01-08 11:13:25 +0530

    Commit APIController/CoreController.cs

---
+++ APIController/CoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace todo.APIController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class CoreController<TEntity> : ControllerBase
+        where TEntity : class
+
+    {
+        private readonly ICosmosDbService<TEntity> _cosmosDbService;
+        public CoreController(ICosmosDbService<TEntity> cosmosDbService)
+        {
+            _cosmosDbService = cosmosDbService;
+        }
+        // GET: api/[controller]
+        [HttpGet]
+        public async Task<IEnumerable<TEntity>> Get()
+        {
+            //return await repository.GetAll();
+            var value = await _cosmosDbService.GetItemsAsync("SELECT * FROM c");
+            return value;
+        }
+
+
+    }
+
+}
---
+++ APIController/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using todo.Models;
+
+namespace todo.APIController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : CoreController<Item>
+    {
+        public TestController(ICosmosDbService<Item> repository) : base(repository)
+        {
+        }
+    }
+}
